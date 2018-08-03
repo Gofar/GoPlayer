5b3390f94b665b6413cfa4254223e6b6f01e864f
lichaofeng <lichaofengfcl@163.com>
2018-08-03 17:56:38 +0800

    Initial commit

---
+++ app/src/main/java/com/gofar/goplayer/PlayerActivity.java
@@ -0,0 +1,11 @@
+package com.gofar.goplayer;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author lcf
+ * @date 1/8/2018 下午 4:02
+ * @since 1.0
+ */
+public class PlayerActivity extends AppCompatActivity{
+}
