dc8c43b511d8b1943c91e73275c1b3b4745e8b10
lichaofeng <lichaofengfcl@163.com>
2018-08-09 14:32:58 +0800

    播放视频

--- app/src/main/java/com/gofar/goplayer/PlayerActivity.java
+++ app/src/main/java/com/gofar/goplayer/PlayerActivity.java
@@ -1,20 +1,26 @@
 package com.gofar.goplayer;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
-import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
-
-import java.util.UUID;
 
 /**
  * @author lcf
@@ -25,25 +31,84 @@ public class PlayerActivity extends AppCompatActivity {
     private PlayerView mPlayerView;
     private SimpleExoPlayer mPlayer;
 
+    private Video mVideo;
+    private MediaSource mMediaSource;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
         setContentView(R.layout.activity_player);
 
         mPlayerView = findViewById(R.id.my_player);
         mPlayerView.setPlayer(mPlayer);
     }
 
-    private void init(){
-        if (mPlayer==null){
-            RenderersFactory factory=new DefaultRenderersFactory(this);
-            TrackSelector trackSelector=new DefaultTrackSelector();
-            LoadControl loadControl=new DefaultLoadControl.Builder().createDefaultLoadControl();
-            String drmScheme="";
-            UUID uuid= Util.getDrmUuid(drmScheme);
-            //DrmSessionManager<FrameworkMediaCrypto> drmSessionManager=new DefaultDrmSessionManager<FrameworkMediaCrypto>(uuid,);
-            //mPlayer=new SimpleExoPlayer(factory,trackSelector,loadControl,drmSessionManager);
+    private void init() {
+        mVideo = getIntent().getParcelableExtra("data");
+        if (mVideo != null) {
+            long w = mVideo.getWidth();
+            long h = mVideo.getHeight();
+            if (w < h) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE);
+            }
         }
+    }
 
+    private void initializePlayer() {
+        if (mPlayer == null) {
+            DefaultRenderersFactory renderersFactory = new DefaultRenderersFactory(this);
+            DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+            TrackSelection.Factory factory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+            DefaultTrackSelector trackSelector = new DefaultTrackSelector(factory);
+            LoadControl loadControl = new DefaultLoadControl();
+            mPlayer = ExoPlayerFactory.newSimpleInstance(renderersFactory, trackSelector, loadControl);
+            mPlayerView.setPlayer(mPlayer);
+            DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                    Util.getUserAgent(this, "com.gofar.goplayer"));
+            mMediaSource = new ExtractorMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(mVideo.getPath()));
+        }
+        mPlayer.prepare(mMediaSource);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Util.SDK_INT <= 23 && mPlayer == null) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void releasePlayer() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 }
